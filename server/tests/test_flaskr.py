ab9702d75e0c492451df75c69074312701f2c429
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2020-10-22 19:15:45 -0400

    Remove failed tests

--- server/tests/test_flaskr.py
+++ server/tests/test_flaskr.py
@@ -3,7 +3,3 @@ from flaskr import create_app
 def test_hello(client):
     response = client.get("/hello")
     assert response.data == b"Hello, World!"
-
-def test_bad(client):
-    response = client.get("/hello")
-    assert response.data == b"Hello"
