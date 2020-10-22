9a5afc8f2197a000f97d11a208183c7f5612005c
Mark Abdullah <markabdullah321@gmail.com>
2020-10-22 19:10:47 -0400

    adding a bad test to test

--- server/tests/test_flaskr.py
+++ server/tests/test_flaskr.py
@@ -3,3 +3,7 @@ from flaskr import create_app
 def test_hello(client):
     response = client.get("/hello")
     assert response.data == b"Hello, World!"
+
+def test_bad(client):
+    response = client.get("/hello")
+    assert response.data == b"Hello"
