594c0b94634ce1e312da0bbb948eadee2fa96fd3
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2020-11-02 18:05:51 -0500

    Add navbar, routing, and basic pages

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByTestId } = render(<App />);
-  const linkElement = getByTestId('test-logo');
-  expect(linkElement).toBeInTheDocument();
+    const { getByTestId } = render(<App />);
+    const linkElement = getByTestId('test-logo');
+    expect(linkElement).toBeInTheDocument();
 });
---
+++ client/src/pages/UserTimline.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function UserTimeline() {
+	return (
+		<h1>This is User Timeline</h1>
+	);
+}
