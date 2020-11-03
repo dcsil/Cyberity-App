27f229abffadcbaccba1ef21895378fc56416db0
Mark <mark.abdullah@utoronto.ca>
2020-11-03 17:42:04 -0500

    Setup inital threats page table

--- client/src/pages/InsiderThreats.js
+++ client/src/pages/InsiderThreats.js
@@ -1,7 +1,84 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
+import Paper from '@material-ui/core/Paper';
 
-export default function InsiderThreats() {
-    return (
-        <h1>This is Insider Threats</h1>
-    );
+const useStyles = makeStyles((theme) => ({
+  table: {
+    minWidth: 650,
+  }
+}));
+
+function createData(name, role, email, status, date, phone) {
+    return { name, role, email, status, date, phone };
+  }
+  
+  const rows = [
+    createData('Mark Abdullah', "Software Dev", "mark@company.com", "Contained", "10/27/2020", "123-456-7890"),
+    createData('Rob Moss', "Software Dev", "rob@company.com", "Threat Detected", "10/28/2020", "123-456-7890"),
+    createData('Vinay Komaravolu', "Software Dev", "vinay@company.com", "Contained", "10/27/2020", "123-456-7890"),
+  ];
+
+function InsiderThreats() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <Paper className={classes.paper}>
+        <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+            <TableRow>
+                <TableCell>User</TableCell>
+                <TableCell>Role</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Date</TableCell>
+                <TableCell>Phone Number</TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            {rows.map((row) => (
+                <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                    {row.name}
+                </TableCell>
+                <TableCell>{row.role}</TableCell>
+                <TableCell>{row.email}</TableCell>
+                <TableCell>{row.status}</TableCell>
+                <TableCell>{row.date}</TableCell>
+                <TableCell>{row.phone}</TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+        </TableContainer>
+        <TablePagination
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+  </Paper>
+  );
 }
+
+export default InsiderThreats; 
