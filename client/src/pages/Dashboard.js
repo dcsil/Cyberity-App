18bd99a20093cb77f043150f1883772f0beb7acf
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2020-11-04 13:20:59 -0500

    Add grid layout functionality

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent';
+import GridLayout from 'react-grid-layout';
+import 'react-grid-layout/css/styles.css';
 
 export default function Dashboard() {
-    return (
-        <h1>This is a dashboard</h1>
-    )
+    const layout = [
+        {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+        {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+        {i: 'c', x: 4, y: 0, w: 1, h: 2}
+      ];
+      return (
+        <GridLayout compactType='horizontal'  className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+            <div key="a">
+                <Card key="a">
+                    <CardContent>
+                        HELLOOO
+                    </CardContent>
+                </Card>
+            </div>
+            <Card key="b">
+                    <CardContent>
+                        HELLOOO
+                    </CardContent>
+                </Card>
+          <div key="c">
+          <Card>
+                    <CardContent>
+                        HELLOOO
+                    </CardContent>
+                </Card>
+          </div>
+        </GridLayout>
+      );
 }
