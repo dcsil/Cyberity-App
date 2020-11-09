33e09a992932617390bc625dcab4265ec23e835e
Mark <mark.abdullah@utoronto.ca>
2020-11-09 11:01:41 -0500

    Added interactive calendar to the dashboard, currently with randomly generated data

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -6,6 +6,7 @@ import ContainedInsiderThreats from '../components/dashboard/ContainedInsiderThr
 import LiveInsiderThreats from '../components/dashboard/LiveInsiderThreats.js';
 import SecurityRating from '../components/dashboard/SecurityRating.js';
 import TruePositives from '../components/dashboard/TruePositives.js';
+import InsiderThreatCalender from '../components/dashboard/InsiderThreatCalender.js';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -27,6 +28,7 @@ export default function Dashboard() {
         {xs:'3', widget: <LiveInsiderThreats/>},
         {xs:'3', widget: <SecurityRating/>},
         {xs:'3', widget: <TruePositives/>},
+        {xs:'12', widget: <InsiderThreatCalender/>},
     ].map((widget) => 
         <Grid item xs={widget.xs}>{widget.widget}</Grid>
     );
