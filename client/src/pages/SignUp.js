ee517dd58cf0b875cc677e766d5f0eef85d8c9c3
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2020-11-20 22:12:29 -0500

    Modify signup layout

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -6,7 +6,6 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import CyberityLogo from '../assets/logo_cyberity_text.png';
-import Paper from '@material-ui/core/Paper';
 import {Link} from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import { useHistory } from "react-router-dom";
@@ -30,6 +29,7 @@ import { useHistory } from "react-router-dom";
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
+      marginTop: "30%"
     },
     avatar: {
       margin: theme.spacing(1),
@@ -42,6 +42,10 @@ import { useHistory } from "react-router-dom";
     submit: {
       margin: theme.spacing(3, 0, 2),
     },
+    auth: {
+        backgroundColor:
+            theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+      }
   }));
   
   export default function SignUp() {
@@ -81,7 +85,7 @@ import { useHistory } from "react-router-dom";
       <Grid container component="main" className={classes.root}>
         <CssBaseline />
         <Grid item xs={false} sm={4} md={7} className={classes.image} />
-        <Grid item xs={12} sm={8} md={5}  component={Paper} elevation={6} square>
+        <Grid item xs={12} sm={8} md={5} className={classes.auth} elevation={6} square>
         <Container component="main">
       <CssBaseline />
       <div className={classes.paper}>
